78f9a1620000911aeb436d208f2c789ad2764e01
ipinky77 <ipinky7@gmail.com>
2021-07-05 17:15:08 +0200

    finalized

--- Install.bat
+++ Install.bat
@@ -1,6 +1,4 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd "${DIR}"
-echo $DIR
+cd /d %~dp0
 npm install bitstamp_trading_tools
 curl -O https://raw.githubusercontent.com/ipinky77/runbitstampTradingTools/main/runBot.js
 curl -O https://raw.githubusercontent.com/ipinky77/runbitstampTradingTools/main/runServer.js
