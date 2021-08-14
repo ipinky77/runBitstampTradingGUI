5e92dbd0391455dbb4d842209ecc78c7ac94b562
ipinky77 <ipinky7@gmail.com>
2021-08-14 16:23:41 +0200

    finetuned profiles, now multiple bots are possible

--- runBot.js
+++ runBot.js
@@ -1,12 +1,23 @@
-const version = "2.0.0"
+const version = "2.0.1"
 console.log("runBot.js", version)
-const getConfiguration = require("bitstamp_trading_tools/config.js")
+const getProfiles = require("bitstamp_trading_tools/config.js")
 const BitstampBot = require("bitstamp_trading_tools/bitstampBot.js")
+var arguments = process.argv;
 
+var config = "configBOT"
+if (2 == arguments.length) {
+    console.log(`We use default configuration "configBOT"`)
+
+} else {
+    config = arguments[2]
+    console.log(`We use configuration ${config}`)
+}
 var bot
 
-getConfiguration(__dirname).then(configuration => {
-    bot = new BitstampBot(configuration.bot)
+getProfiles(__dirname, "BOT").then(profiles => {
+    console.log("profiles", profiles)
+    console.log("config", config)
+    bot = new BitstampBot(profiles[config])
 
     runBot()
 
@@ -19,5 +30,3 @@ async function runBot() {
     await bot.init()
     await bot.run()
 }
-
-
